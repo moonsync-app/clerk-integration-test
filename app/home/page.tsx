861a67ff8fbd3a4f1dc945b10beacc3cc66324a1
Rui Chen <rui@chenrui.dev>
2024-05-10 21:51:44 -0400

    add clerk test pages
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ app/home/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>test app</h1>
+      <p>Testing clerk integration</p>
+    </div>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default async function Page() {
+  return (
+    <main>
+      <h1>test app</h1>
+      <p>Testing clerk integration</p>
+    </main>
+  );
+}
